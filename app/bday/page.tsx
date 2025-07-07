7490995216dbe22371e8227bced6f16ceef29ec5
nandhu <nandhu@nandhus-MacBook-Air.local>
2025-07-08 00:09:27 +0530

    Initial commit

---
+++ app/bday/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+import Countdown from '@/components/elements/Countdown'
+import Layout from "@/components/layout/Layout"
+import Link from "next/link"
+
+export default function Memories() {
+    return (
+        <Layout headerStyle={1} footerStyle={1}>
+            <div>
+                {/* MEMORY SECTION */}
+                <div className="memory-inner-section-area sp1">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-4 col-md-6">
+                                <div className="memory3-boxarea">
+                                    <div className="img1">
+                                        <img src="/assets/img/all-images/bday/img1.png" alt="memory" loading="lazy" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-6">
+                                <div className="memory3-boxarea">
+                                    <div className="img1">
+                                        <img src="/assets/img/all-images/bday/img2.png" alt="memory" loading="lazy" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-6">
+                                <div className="memory3-boxarea">
+                                    <div className="img1">
+                                        <img src="/assets/img/all-images/bday/img3.png" alt="memory" loading="lazy" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-6">
+                                <div className="memory3-boxarea">
+                                    <div className="img1">
+                                        <img src="/assets/img/all-images/bday/img4.png" alt="memory" loading="lazy" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-6">
+                                <div className="memory3-boxarea">
+                                    <div className="img1">
+                                        <img src="/assets/img/all-images/bday/img5.png" alt="memory" loading="lazy" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-6">
+                                <div className="memory3-boxarea">
+                                    <div className="img1">
+                                        <img src="/assets/img/all-images/bday/img6.png" alt="memory" loading="lazy" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-6">
+                                <div className="memory3-boxarea">
+                                    <div className="img1">
+                                        <img src="/assets/img/all-images/bday/img7.png" alt="memory" loading="lazy" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-6">
+                                <div className="memory3-boxarea">
+                                    <div className="img1">
+                                        <img src="/assets/img/all-images/bday/img8.png" alt="memory" loading="lazy" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-6">
+                                <div className="memory3-boxarea">
+                                    <div className="img1">
+                                        <img src="/assets/img/all-images/bday/img9.png" alt="memory" loading="lazy" />
+                                    </div>
+                                </div>
+                            </div>
+                           
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+               
+        </Layout>
+    )
+}
